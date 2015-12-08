449f8f27555c749c757c23565002b4d2b8c40c26
Joey Frohlinger <jmafrohlinger@outlook.com>
2015-12-08 10:55:40 -0800

    created simple received test for azure service bus q

---
+++ ServiceBusQPullTest/ServiceBusQPullTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using Microsoft.ServiceBus;
+using Microsoft.WindowsAzure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBusQPullTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+            QueueClient Client = QueueClient.CreateFromConnectionString(connectionString, "testvector", ReceiveMode.ReceiveAndDelete);
+            var ns = NamespaceManager.CreateFromConnectionString(connectionString);
+
+            var qLength = ns.GetQueue("testvector").MessageCount; //requires manager privilage... uch
+
+
+            OnMessageOptions options = new OnMessageOptions();
+            options.AutoComplete = true;
+            options.AutoRenewTimeout = TimeSpan.FromMinutes(1);
+
+            if (qLength > 0)
+            {
+                var message = Client.Receive();
+                //if receivemode above is peek, must call message.complete to remove from queue
+            }
+
+        }
+    }
+}
