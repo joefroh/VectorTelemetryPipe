66fec2adcd1c2a6c23fb5d1f8576252e9822efd8
Joey Frohlinger <jmafrohlinger@outlook.com>
2015-12-08 11:06:53 -0800

    simplest vector writer library for c#

---
+++ VectorLib/VectorLib/Vector.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using Microsoft.WindowsAzure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorLib
+{
+    public class Vector
+    {
+        public string ConnectionString { get; private set; }
+        private QueueClient QClient;
+
+        private const string QName = "vectormain";
+
+        public Vector()
+        {
+            ConnectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+            QClient = QueueClient.CreateFromConnectionString(ConnectionString, QName, ReceiveMode.ReceiveAndDelete);
+        }
+
+        public void WriteMessage(string message)
+        {
+            BrokeredMessage msg = new BrokeredMessage();
+            msg.Properties["message-content"] = message;
+
+            QClient.Send(msg);
+        }
+    }
+}
