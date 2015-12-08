c76684bf20134efe61a18a3a877c8130c582884f
Joey Frohlinger <jmafrohlinger@outlook.com>
2015-12-08 11:33:24 -0800

    Super Simple E2E working

---
+++ VectorLib/VectorTestApplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VectorLib;
+namespace VectorTestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("inputText.txt");
+            var words = reader.ReadLine().Split(' ');
+
+            var vector = new Vector();
+
+            foreach (var word in words)
+            {
+                vector.WriteMessage(word);
+            }
+        }
+    }
+}
---
+++ VectorPullService/VectorPullService/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.WindowsAzure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VectorPullService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+            QueueClient Client = QueueClient.CreateFromConnectionString(connectionString, "vectormain", ReceiveMode.ReceiveAndDelete);
+           
+            OnMessageOptions options = new OnMessageOptions();
+            options.AutoComplete = true;
+            options.AutoRenewTimeout = TimeSpan.FromMinutes(1);
+
+            Client.OnMessage(MessageHandler, options);
+
+            Console.WriteLine("====Listening for messages. Press ENTER to quit.====");
+            Console.ReadLine();
+        }
+
+        private static void MessageHandler(BrokeredMessage msg)
+        {
+            Console.WriteLine(msg.Properties["message-content"]);
+        }
+    }
+}
