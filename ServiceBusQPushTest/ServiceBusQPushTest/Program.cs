16d65dcbe53383c31460cfdb3736dd82d27a0248
Joey Frohlinger <jmafrohlinger@outlook.com>
2015-12-08 10:42:56 -0800

    Build simple sender to test queue

---
+++ ServiceBusQPushTest/ServiceBusQPushTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using Microsoft.WindowsAzure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBusQPushTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+            QueueClient Client = QueueClient.CreateFromConnectionString(connectionString, "testvector");
+
+            Client.Send(new BrokeredMessage() { });
+        }
+    }
+}
